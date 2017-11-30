64c5fc5fb788e42fb7ecb4ee3ee3a40052072858
Justin Knueppel <justinknueppel@gmail.com>
2017-11-30 11:12:03 -0500

    add ranges of ASCII characters to Character Classes

--- Regular Expressions/CharacterClasses.py
+++ Regular Expressions/CharacterClasses.py
@@ -9,3 +9,11 @@ if re.search(pattern1, "qwertyuio"): #matches
 	print('1Match 2')
 if re.search(pattern1, "rhythm"): #no match
 	print('1Match 3')
+print()
+
+#you can use ranges within character classes based on ASCII encoding
+pattern2 = r"[a-z]" #or [A-Z], [0-9]
+if re.search(pattern2, "Hello World"): #contains lower case letter(s)
+	print('2Match 1')
+if re.search(pattern2, "HELLO WORLD"): #all caps, wont match
+	print('2Match 2')
